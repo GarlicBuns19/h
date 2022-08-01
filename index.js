13e7a4fc05dbe069ac1f8ae4215793e625b56551
GarlicBuns19 <planterx26@gmail.com>
2022-08-01 22:14:56 +0100

    1

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const arr =[
     name:'Working'
   }
 ]
-const router = express.Router();
 app.listen(process.env.PORT || 5000);
 
-router.get('/arr', (req, res)=> {
+app.get('/arr', (req, res)=> {
   res.send(arr);
 });
 
