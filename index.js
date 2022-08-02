b27c03b7ef7b0aa42a25c35fbc7677dea47ac899
GarlicBuns19 <planterx26@gmail.com>
2022-08-02 06:28:16 +0100

    Update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
-const data = require('./data.json')
+const data = require("./data.json");
 let port = process.env.PORT || 5000;
-app.use(express.json())
-app.listen(port, (req,res) => {
-  console.log(`Running port http://localhost:${port}`)
+app.use(express.json());
+
+const cars =
+    {
+      id: 1,
+      name: "Hello",
+    };
+
+app.listen(port, (req, res) => {
+  console.log(`Running port http://localhost:${port}`);
 });
 
-app.get('/', (req, res)=> {
-  res.send(data);
+app.get("/", (req, res) => {
+  // res.send(console.log(cars));
+  res.send(data.cars['Ford'].map(e => e.name));
 });
-
