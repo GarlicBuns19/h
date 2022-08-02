cd59f3de59f9cdc60c0fafe778718d98377da17b
GarlicBuns19 <planterx26@gmail.com>
2022-08-02 05:53:59 +0100

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const data = require('./data.json')
 let port = process.env.PORT || 5000;
-
+app.use(express.json())
 app.listen(port, (req,res) => {
   console.log(`Running port http://localhost:${port}`)
 });
